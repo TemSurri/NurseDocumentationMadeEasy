75e9c75a7697aa254ef63ef2c35fbd7cbf0343be
tembatsurri <tarantem@icloud.com>
2025-10-31 10:28:03 -0700

    tesing with the router

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -1,15 +1,24 @@
 import Header from "./components/header"
 import Hero from "./components/hero"
 import Footer from "./components/footer"
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow">
-        <Hero />
+      <Router> {/* main router area   */}
+        <Routes>
+          <Route path='hi' element= {<div>Hi</div>}/>
+          <Route path='/' element={<Hero/>}/>
+        </Routes>
+      </Router>
+
       </main>
       <Footer />
     </div>
+
   )
 }
