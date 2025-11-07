5ac8014c79f6d9c43da90b5c4676fc92acc5be12
Tem Batsuuri <129964363+TemSurri@users.noreply.github.com>
2025-11-07 14:08:36 -0800

    Revert "created functional recording component and notes manager class"

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import Header from "./components/header"
 import Hero from "./components/hero"
 import Footer from "./components/footer"
 import Interface from "./components/recording_interface"
-import Recorder from "./components/recorder"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import About from "./components/about.tsx"
 
@@ -16,7 +15,6 @@ export default function App() {
           <Route path='/record' element= {<Interface />}/>
           <Route path='/' element={<Hero/>}/>
           <Route path='/about' element={<About/>}/>
-          <Route path='/r' element={<Recorder/>}/>
         </Routes>
       </Router>
 
