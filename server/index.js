3f854695a0c02206add50132339bf320e0012f11
tembatsurri <tarantem@icloud.com>
2025-11-12 12:17:02 -0800

    testing on vercel

--- server/index.js
+++ server/index.js
@@ -8,12 +8,49 @@ const corsOptions = {
 const app = express();
 app.use(cors(corsOptions));
 const PORT = 3001;
-const upload = multer({dest:"uploads"});
+const storage = multer.memoryStorage();
+const upload = multer({storage})
+
+
+queue = []
+let processing = false;
+
+async function processQueue() {
+  if (processing || queue.length === 0) return;
+  processing = true;
+
+  const {file, res} = queue.shift();
+
+  try {
+    // open ai api call and get thetranscript 
+    transcript = "open ai touched";
+    res.json({sucess:true, transcript});
+  }catch (error){
+    console.log(error);
+    res.status(500).json({success: false, error: "Processing failed"})
+  } finally {
+    processing = false;
+    processQueue();
+  }
+
+}
+
 
 app.post('/upload-audio', upload.single("audio"), async(req, res)=> {
+  if (!req.file) return res.status(400).json({error: "No file uploaded"});
 
-  res.json({req : req.file , transcript : "This is a fake transcription for testing"})
-  
+  queue.push({file: req.file, res})
+
+  console.log("Added file to queue:", {
+  originalname: req.file.originalname,
+  mimetype: req.file.mimetype,
+  size: req.file.size,
+  queuelength : queue.length
+  });
+
+
+  processQueue();
+
 })
 
 
